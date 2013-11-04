41243647658e6035302b2333d4c49f3221baf372
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-04 05:09:50 -0800

    create chapter 6 notes for Learn to Program

---
+++ LTPNotes_chapt6.rb
@@ -0,0 +1,18 @@
+# The First Ten Methods in Action
+# From "Learn to Program" by Chris Pine
+#
+
+puts "These are the ten methods we know so far:"
+puts "puts, gets, chomp"
+puts "See, we used puts right there."
+puts "Y or N"
+response = gets.chomp()
+puts "Now here's your response with .to_i:"
+puts "#{response.to_i}"
+puts "Now with .to_s"
+puts "#{response.to_s}"
+puts
+puts "The last four methods are the operations:"
+puts " + - * // "
+puts
+puts "As a side note, this book is hilarious!"
