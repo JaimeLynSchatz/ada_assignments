69b5442c96132d6e5f9fe105b76218f0bf273712
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-04 05:14:46 -0800

    update chapt6 notes

--- LTPNotes_chapt6.rb
+++ LTPNotes_chapt6.rb
@@ -1,6 +1,10 @@
 # The First Ten Methods in Action
 # From "Learn to Program" by Chris Pine
 #
+puts "Where am I, anyway?"
+puts self
+puts "Oh, puts self. Thanks"
+puts
 
 puts "These are the ten methods we know so far:"
 puts "puts, gets, chomp"
@@ -15,4 +19,21 @@ puts
 puts "The last four methods are the operations:"
 puts " + - * // "
 puts
+
 puts "As a side note, this book is hilarious!"
+puts
+
+# from page 35 in LTP
+puts "FANCY STRING METHODS AHOY!"
+var1 = 'stop'
+var2 = 'deliver rapid desserts'
+var3 = '....TCELES B HSUP  A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+
