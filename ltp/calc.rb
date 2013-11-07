f8a950aa14434922a01ee5dc9fa1e1fd09168d63
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-07 12:02:54 -0800

    did it really take 7 years?

--- ltp/calc.rb
+++ ltp/calc.rb
@@ -14,7 +14,7 @@ HOURS_PER_DAY = 24
 DAYS_PER_YEAR = 365.25
 YEARS_PER_DECADE = 10
 MY_AGE = 36 # ha ha, if only it were a constant
-AUTHOR_AGE_IN_SEC = 1025000000000.0
+AUTHOR_AGE_IN_SEC = 1025000000.0
 START_AGE = 800000000.0
 
 SEC_IN_YEAR = SEC_PER_MIN * MIN_PER_HOUR * HOURS_PER_DAY * DAYS_PER_YEAR
@@ -30,4 +30,4 @@ puts "I am #{AGE_IN_SECONDS} seconds old, give or take."
 puts
 puts "The author is #{AUTHOR_AGE_IN_SEC / SEC_IN_YEAR} years old."
 puts
-puts "It took him #{AUTHOR_AGE_IN_SEC - START_AGE / SEC_IN_YEAR} years to write the book."
+puts "It took him #{(AUTHOR_AGE_IN_SEC - START_AGE) / SEC_IN_YEAR} years to write the book."
