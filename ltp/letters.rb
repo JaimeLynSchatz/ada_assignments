8eb7811fb43c4c285d3b9215be88ab80409d94d7
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-07 12:25:33 -0800

    concatenation has limits

--- ltp/letters.rb
+++ ltp/letters.rb
@@ -7,9 +7,14 @@ puts 'blink ' * 4 # batting her eyes - use this for Bob!!
 puts 12 + 12
 puts '12' + '12'
 puts '12 + 12'
-puts 'What do you get whe you add 1 and 1? Eleven. Yuk yuk yuk.'
+puts 'What do you get when you add 1 and 1? Eleven. Yuk yuk yuk.'
 
 puts 2 * 5
 puts '2' * 5
 puts '2 * 5'
+
+# you can't do that on television
+# puts '12' + 12
+# you can't do that on television, either
+# puts '2' * '5'
 
