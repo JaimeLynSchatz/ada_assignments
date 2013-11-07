f5297fafa3f951404ad6a0d4084dc83e542a5fa7
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2013-11-07 12:09:48 -0800

    starting chapter 3

---
+++ ltp/letters.rb
@@ -0,0 +1,15 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4 # batting her eyes - use this for Bob!!
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 'What do you get whe you add 1 and 1? Eleven. Yuk yuk yuk.'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
