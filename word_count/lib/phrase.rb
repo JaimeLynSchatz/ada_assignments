5198325583263001d61dbc7c8ad71c826b3bb73d
JaimeLynSchatz <jaimelynschatz@gmail.com>
2013-11-23 05:20:42 -0800

    warm-up as project, sigh

--- word_count/lib/phrase.rb
+++ word_count/lib/phrase.rb
@@ -3,7 +3,22 @@
 class Phrase
   attr_accessor :str
   def initialize(str)
-    
+    @str = str
+    @str
+  end
+
+  def word_count
+    words = @str.split(" ")
+    counts = {}
+    words.each do |word|
+      if counts.has_value?(word)
+        words[word] =+ 1
+      else
+        words[word] = 1
+      end
+    end
+    counts
+  end
 end
 
 phrase = Phrase.new("olly olly come in free")
